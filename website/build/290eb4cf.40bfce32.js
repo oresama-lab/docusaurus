8029ebe1213e91d3e8452ff069a8ec5ebc871880
Masayoshi Tohna <oresamalabo@gmail.com>
2021-02-21 10:34:55 +0900

    build: test build

---
+++ website/build/290eb4cf.40bfce32.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{77:function(e){e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Welcome","permalink":"/docusaurus/blog/welcome"},{"title":"Hello","permalink":"/docusaurus/blog/hello-world"},{"title":"Hola","permalink":"/docusaurus/blog/hola"}]}')}}]);
